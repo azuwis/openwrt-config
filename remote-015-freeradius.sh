9da42fdfa4e2b250578618b9334a29c46ae548d5
Zhong Jianxin <azuwis@gmail.com>
2017-03-10 21:48:57 +0800

    freeradius: Add eap handling, remove some packages

--- remote-015-freeradius.sh
+++ remote-015-freeradius.sh
@@ -4,8 +4,8 @@ freeradius_packages() {
     if ! pgrep -x /usr/sbin/radiusd >/dev/null
     then
         [ -z "$config_freeradius_ca" ] && oc_opkg_install freeradius3-democerts
-        oc_opkg_install freeradius3 freeradius3-mod-always freeradius3-mod-attr-filter freeradius3-mod-chap freeradius3-mod-detail freeradius3-mod-digest freeradius3-mod-eap-gtc freeradius3-mod-eap-leap freeradius3-mod-eap-md5 freeradius3-mod-eap-mschapv2 freeradius3-mod-eap-peap freeradius3-mod-eap-tls freeradius3-mod-exec freeradius3-mod-expiration freeradius3-mod-expr freeradius3-mod-files freeradius3-mod-logintime freeradius3-mod-mschap freeradius3-mod-pap freeradius3-mod-preprocess freeradius3-mod-radutmp freeradius3-mod-realm freeradius3-mod-unix
-        oc_opkg_installed freeradius3-mod-eap-ttls || opkg install --force-overwrite freeradius3-mod-eap-ttls
+        oc_opkg_install freeradius3 freeradius3-mod-always freeradius3-mod-attr-filter freeradius3-mod-chap freeradius3-mod-detail freeradius3-mod-digest freeradius3-mod-eap-tls freeradius3-mod-exec freeradius3-mod-expiration freeradius3-mod-expr freeradius3-mod-files freeradius3-mod-logintime freeradius3-mod-mschap freeradius3-mod-pap freeradius3-mod-preprocess freeradius3-mod-radutmp freeradius3-mod-realm freeradius3-mod-unix
+        [ "$config_freeradius_eap_peap_enabled" = 1 ] && oc_opkg_install freeradius3-mod-eap-mschapv2 freeradius3-mod-eap-peap
     fi
 }
 
@@ -55,6 +55,54 @@ freeradius_certs() {
     chmod 640 /etc/freeradius3/certs/*
 }
 
+freeradius_eap() {
+    cat >/tmp/freeradius-eap.conf <<EOF
+eap {
+  default_eap_type = tls
+  timer_expire = 60
+  ignore_unknown_eap_types = no
+  cisco_accounting_username_bug = no
+  max_sessions = \${max_requests}
+  tls-config tls-common {
+    private_key_password =
+    private_key_file = \${certdir}/server.pem
+    certificate_file = \${certdir}/server.pem
+    ca_file = \${cadir}/ca.pem
+    dh_file = \${certdir}/dh
+    ca_path = \${cadir}
+    check_cert_cn = %{User-Name}
+    cipher_list = "HIGH"
+    ecdh_curve = "secp384r1"
+    cache {
+      enable = yes
+      lifetime = 24 # hours
+      max_entries = 255
+    }
+  }
+  tls {
+    tls = tls-common
+  }
+EOF
+    if [ "$config_freeradius_eap_peap_enabled" = 1 ]
+    then
+        cat >>/tmp/freeradius-eap.conf <<EOF
+  peap {
+    tls = tls-common
+    default_eap_type = mschapv2
+    copy_request_to_tunnel = no
+    use_tunneled_reply = no
+    virtual_server = "inner-tunnel"
+  }
+  mschapv2 {
+  }
+EOF
+    fi
+    cat >>/tmp/freeradius-eap.conf <<EOF
+}
+EOF
+    oc_move /tmp/freeradius-eap.conf /etc/freeradius3/mods-available/eap && radiusd_need_restart=1
+}
+
 freeradius_users() {
     local rc=1
     if oc_opkg_installed freeradius3-mod-files
@@ -82,5 +130,6 @@ radiusd_need_restart=0
 freeradius_packages
 freeradius_clients
 freeradius_certs
+freeradius_eap
 echo "$config_freeradius_users" | oc_strip_comment | freeradius_users && radiusd_need_restart=1
 freeradius_service
