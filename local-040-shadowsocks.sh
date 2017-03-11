8275e54767337a6779b49ae9ee2ca64322fe2ed1
Zhong Jianxin <azuwis@gmail.com>
2017-03-11 21:07:37 +0800

    shadowsocks: Keep files in files/shadowsocks/

--- local-040-shadowsocks.sh
+++ local-040-shadowsocks.sh
@@ -1,14 +1,14 @@
 chn_cidr() {
     awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }'
 }
-if [ ! -e files/chn-cidr ]; then
+if [ ! -e files/shadowsocks/chn-cidr ]; then
     download http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
-    chn_cidr < files/tmp/delegated-apnic-latest > files/chn-cidr
-    cat >> files/chn-cidr <<EOF
+    chn_cidr < files/tmp/delegated-apnic-latest > files/shadowsocks/chn-cidr
+    cat >> files/shadowsocks/chn-cidr <<EOF
 64.62.200.2/32
 1.1.1.0/24
 EOF
 fi
-push files/chn-cidr /etc/chn-cidr
-push files/shadowsocks.keep /lib/upgrade/keep.d/shadowsocks
-push files/ss /root/bin/ss
+push files/shadowsocks/chn-cidr /etc/chn-cidr
+push files/shadowsocks/keep /lib/upgrade/keep.d/shadowsocks
+push files/shadowsocks/ss /root/bin/ss
