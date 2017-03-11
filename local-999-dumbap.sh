73cfd08120d2e1a1a73bd30ecfedea4c1d6041ff
Zhong Jianxin <azuwis@gmail.com>
2017-03-11 21:09:53 +0800

    dumbap: Keep files in files/dumbap/

--- local-999-dumbap.sh
+++ local-999-dumbap.sh
@@ -1,2 +1,2 @@
-push files/99-dumbap /etc/uci-defaults/99-dumbap
-push files/dumbap.keep /lib/upgrade/keep.d/dumbap
+push files/dumbap/uci-defaults /etc/uci-defaults/99-dumbap
+push files/dumbap/keep /lib/upgrade/keep.d/dumbap
