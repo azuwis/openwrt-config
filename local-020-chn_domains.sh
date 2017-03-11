bd4cc8d127f40452b07fdcf47c2f7163bc8bc2c9
Zhong Jianxin <azuwis@gmail.com>
2017-03-11 21:00:22 +0800

    chn_domains: Keep files in files/chn_domains

--- local-020-chn_domains.sh
+++ local-020-chn_domains.sh
@@ -2,18 +2,18 @@ chn_domains() {
     sed -e 's!^server=/!!' -e 's!/114.114.114.114$!!' | grep -Fv cn.debian.org
 }
 
-if [ ! -e files/chn-domains ]; then
+if [ ! -e files/chn_domains/chn-domains ]; then
     download https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
-    chn_domains < files/tmp/accelerated-domains.china.conf > files/chn-domains
+    chn_domains < files/tmp/accelerated-domains.china.conf > files/chn_domains/chn-domains
 fi
 
-if [ ! -e files/tmp/chn-domains.gz -o files/chn-domains -nt files/tmp/chn-domains.gz ]; then
-    echo 'zipping files/chn-domains'
-    gzip -nc files/chn-domains > files/tmp/chn-domains.gz
+if [ ! -e files/tmp/chn-domains.gz -o files/chn_domains/chn-domains -nt files/tmp/chn-domains.gz ]; then
+    echo 'zipping files/chn_domains/chn-domains'
+    gzip -nc files/chn_domains/chn-domains > files/tmp/chn-domains.gz
 fi
 
 push files/tmp/chn-domains.gz /etc/chn-domains.gz
-push files/chn-domains-extra /etc/chn-domains-extra
-push files/99-chn-domains /etc/hotplug.d/iface/99-chn-domains
+push files/chn_domains/chn-domains-extra /etc/chn-domains-extra
+push files/chn_domains/hotplug /etc/hotplug.d/iface/99-chn-domains
 
-push files/chn-domains.keep /lib/upgrade/keep.d/chn-domains
+push files/chn_domains/keep /lib/upgrade/keep.d/chn-domains
