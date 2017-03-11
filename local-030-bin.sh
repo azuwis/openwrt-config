df13dc5eb745257ad045a44a039c666b106752aa
Zhong Jianxin <azuwis@gmail.com>
2017-03-11 21:04:13 +0800

    bin: Keep files in files/bin/

--- local-030-bin.sh
+++ local-030-bin.sh
@@ -1,5 +1,5 @@
 remote mkdir -p /etc/profile.d/
 remote mkdir -p /root/bin/
 
-push files/bin.keep /lib/upgrade/keep.d/bin
-push files/profile.sh /etc/profile.d/bin.sh
+push files/bin/keep /lib/upgrade/keep.d/bin
+push files/bin/profile.sh /etc/profile.d/bin.sh
