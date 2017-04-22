89f09aeb4c0cc39ebb75bfe241709db983e64603
Zhong Jianxin <azuwis@gmail.com>
2017-04-23 01:07:58 +0800

    Add ipinfo and ipcn bin

--- local-030-bin.sh
+++ local-030-bin.sh
@@ -3,3 +3,6 @@ remote mkdir -p /root/bin/
 
 push files/bin/keep /lib/upgrade/keep.d/bin
 push files/bin/profile.sh /etc/profile.d/bin.sh
+
+push files/bin/ipcn /root/bin/ipcn
+push files/bin/ipinfo /root/bin/ipinfo
