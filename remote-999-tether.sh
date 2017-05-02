4680fa65ac1a1c35ec950d64e45c0b089e3d9501
Zhong Jianxin <azuwis@gmail.com>
2017-05-02 10:32:14 +0800

    tether: Separate rndis and wlan handling

--- remote-999-tether.sh
+++ remote-999-tether.sh
@@ -1,26 +1,50 @@
-tether() {
+tether_rndis() {
     oc_opkg_install kmod-usb-net-rndis
+
+    local iface wan
+    iface="$1"
+    wan="$2"
 
     uci batch <<EOF
 set network.wan.metric=10
-
-set network.twan1=interface
-set network.twan1.ifname=usb0
-set network.twan1.proto=dhcp
-set network.twan1.ipv6=0
-set network.twan1.metric=20
-
-set network.twan2=interface
-set network.twan2.proto=dhcp
-set network.twan2.ipv6=0
-set network.twan2.metric=30
+set network.${wan}=interface
+set network.${wan}.ifname=${iface}
+set network.${wan}.proto=dhcp
+set network.${wan}.ipv6=0
+set network.${wan}.metric=20
 EOF
-    oc_uci_batch_set "$config_tether"
-
     oc_service reload network
 
-    oc_uci_add_list 'firewall.@zone[1].network' twan1 twan2
+    oc_uci_add_list 'firewall.@zone[1].network' "$wan"
     oc_service reload firewall 2>/dev/null
 }
 
-tether
+tether_wlan() {
+    local iface wan
+    iface="$1"
+    wan="$2"
+
+    uci batch <<EOF
+set network.wan.metric=10
+set network.${wan}=interface
+set network.${wan}.proto=dhcp
+set network.${wan}.ipv6=0
+set network.${wan}.metric=30
+set wireless.${iface}.network=twan2
+EOF
+    uci commit wireless
+    oc_service reload network
+
+    oc_uci_add_list 'firewall.@zone[1].network' "$wan"
+    oc_service reload firewall 2>/dev/null
+}
+
+if [ -n "$config_tether_rndis" ]
+then
+    tether_rndis "$config_tether_rndis" twan1
+fi
+
+if [ -n "$config_tether_wlan" ]
+then
+    tether_wlan "$config_tether_wlan" twan2
+fi
