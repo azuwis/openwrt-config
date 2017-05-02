d496d1c2a949cb6e60e4eaf86bf777d5a80b547e
Zhong Jianxin <azuwis@gmail.com>
2017-05-02 10:13:05 +0800

    tether: Rename wan to twan

--- remote-999-tether.sh
+++ remote-999-tether.sh
@@ -4,22 +4,22 @@ tether() {
     uci batch <<EOF
 set network.wan.metric=10
 
-set network.wan1=interface
-set network.wan1.ifname=usb0
-set network.wan1.proto=dhcp
-set network.wan1.ipv6=0
-set network.wan1.metric=20
+set network.twan1=interface
+set network.twan1.ifname=usb0
+set network.twan1.proto=dhcp
+set network.twan1.ipv6=0
+set network.twan1.metric=20
 
-set network.wan2=interface
-set network.wan2.proto=dhcp
-set network.wan2.ipv6=0
-set network.wan2.metric=30
+set network.twan2=interface
+set network.twan2.proto=dhcp
+set network.twan2.ipv6=0
+set network.twan2.metric=30
 EOF
     oc_uci_batch_set "$config_tether"
 
     oc_service reload network
 
-    oc_uci_add_list 'firewall.@zone[1].network' wan1 wan2
+    oc_uci_add_list 'firewall.@zone[1].network' twan1 twan2
     oc_service reload firewall 2>/dev/null
 }
 
