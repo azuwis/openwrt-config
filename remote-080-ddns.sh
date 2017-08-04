e0d66aeeb97fafec79939086e3dfa05c5b6e5c88
Zhong Jianxin <azuwis@gmail.com>
2017-08-04 14:55:52 +0800

    ddns: Use oc_uci_merge

--- remote-080-ddns.sh
+++ remote-080-ddns.sh
@@ -4,5 +4,5 @@ oc_uci_delete ddns.myddns_ipv4 ddns.myddns_ipv6
 # uci batch <<EOF
 # set ddns.global.use_curl='1'
 # EOF
-oc_uci_batch_set "$config_ddns"
+oc_uci_merge ddns "$config_ddns"
 oc_service restart ddns
