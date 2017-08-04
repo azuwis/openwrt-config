271107bbc7bc256127a6f006c4c70e652e1af077
Zhong Jianxin <azuwis@gmail.com>
2017-08-04 16:49:12 +0800

    ss: Use uci import

--- remote-110-shadowsocks.sh
+++ remote-110-shadowsocks.sh
@@ -1,7 +1,6 @@
 oc_opkg_install shadowsocks-libev-ss-rules
 
-oc_uci_batch_set "$config_shadowsocks_common"
-oc_uci_batch_set "$config_shadowsocks"
+echo "$config_shadowsocks" | oc_strip_comment | uci import shadowsocks-libev
 oc_service restart shadowsocks-libev
 
 if [ /etc/chn-cidr -nt /var/etc/shadowsocks-libev.json ]; then
