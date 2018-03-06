ad43a9015654e6ab7f3342f93c8c9c711b97c976
Zhong Jianxin <azuwis@gmail.com>
2018-03-06 13:18:26 +0800

    bin: Use oc- prefix to avoid potential conflict with upstream

--- local-030-bin.sh
+++ local-030-bin.sh
@@ -1,8 +1,8 @@
 remote mkdir -p /etc/profile.d/
 remote mkdir -p /root/bin/
 
-push files/bin/keep /lib/upgrade/keep.d/bin
-push files/bin/profile.sh /etc/profile.d/bin.sh
+push files/bin/keep /lib/upgrade/keep.d/oc-bin
+push files/bin/profile.sh /etc/profile.d/oc-bin.sh
 
 push files/bin/ipcn /root/bin/ipcn
 push files/bin/ipinfo /root/bin/ipinfo
