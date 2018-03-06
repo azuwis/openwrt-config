1ca1642ed024b513fdbfab1e26a3c74d9005a943
Zhong Jianxin <azuwis@gmail.com>
2018-03-06 10:36:09 +0800

    adblock: Sync with upstream update

--- remote-070-adblock.sh
+++ remote-070-adblock.sh
@@ -1,7 +1,6 @@
 oc_opkg_install adblock ca-bundle libustream-openssl
 
-uci set "adblock.global.adb_fetch=/bin/uclient-fetch"
-uci set "adblock.global.adb_fetchparm=-q --timeout=5 -O"
+uci set "adblock.global.adb_enabled=1"
 oc_service restart adblock
 
 oc_add_cron adblock '0 4 * * * /etc/init.d/adblock start'
