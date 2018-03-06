94bfb63cbf33088b91240569f0b22e31bb1255ae
Zhong Jianxin <azuwis@gmail.com>
2018-03-06 13:35:37 +0800

    freeradius: Add number prefix to uci-defaults

--- local-015-freeradius.sh
+++ local-015-freeradius.sh
@@ -1,2 +1,2 @@
-push files/freeradius/uci-defaults /etc/uci-defaults/oc-freeradius
+push files/freeradius/uci-defaults /etc/uci-defaults/99-oc-freeradius
 push files/freeradius/keep /lib/upgrade/keep.d/oc-freeradius
