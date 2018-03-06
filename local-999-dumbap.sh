62744df00d851d53a10376c2cbfa9aae049e0f12
Zhong Jianxin <azuwis@gmail.com>
2018-03-06 13:30:28 +0800

    dumpap: Use oc- prefix to avoid potential conflict with upstream

--- local-999-dumbap.sh
+++ local-999-dumbap.sh
@@ -1,2 +1,2 @@
-push files/dumbap/uci-defaults /etc/uci-defaults/99-dumbap
-push files/dumbap/keep /lib/upgrade/keep.d/dumbap
+push files/dumbap/uci-defaults /etc/uci-defaults/99-oc-dumbap
+push files/dumbap/keep /lib/upgrade/keep.d/oc-dumbap
