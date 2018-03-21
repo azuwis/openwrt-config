46db11d52ff5f768f0987ec8eae5300866dfd2da
Zhong Jianxin <azuwis@gmail.com>
2018-03-21 14:54:20 +0800

    adblock: Disable config for now

--- remote-070-adblock.sh
+++ remote-070-adblock.sh
@@ -1,6 +1,6 @@
 oc_opkg_install adblock ca-bundle libustream-openssl
 
-uci set "adblock.global.adb_enabled=1"
-oc_service restart adblock
+# uci set "adblock.global.adb_enabled=1"
+# oc_service restart adblock
 
 oc_add_cron adblock '0 4 * * * /etc/init.d/adblock start'
