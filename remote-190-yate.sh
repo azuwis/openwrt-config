856edcd5a7acb411940166f27d2963ed7db6706e
Zhong Jianxin <azuwis@gmail.com>
2018-04-02 16:16:08 +0800

    yate: Support TLS

--- remote-190-yate.sh
+++ remote-190-yate.sh
@@ -1,11 +1,17 @@
 yate_packages() {
-    oc_opkg_install yate yate-mod-ysipchan yate-mod-yrtpchan yate-mod-regfile yate-mod-regexroute
+    oc_opkg_install yate yate-mod-regexroute yate-mod-regfile yate-mod-yrtpchan yate-mod-ysipchan
+    [ "$config_yate_sip_type" = 'tls' ] && oc_opkg_install yate-mod-openssl
 }
 
 yate_ysipchan() {
+    local secure
+    [ "$config_yate_sip_type" = 'tls' ] && secure='yes' || secure='no'
     cat >/tmp/yate-ysipchan.conf <<EOF
 [general]
 port=${config_yate_sip_port:-5060}
+type=${config_yate_sip_type:-udp}
+sslcontext=server
+secure=${secure}
 useragent=${config_yate_sip_useragent:-YATE/2.0.0}
 realm=${config_yate_sip_realm:-Yate}
 auth_foreign=yes
@@ -32,6 +38,22 @@ minport=${config_yate_rtp_minport:-10000}
 maxport=${config_yate_rtp_maxport:-20000}
 EOF
     oc_move /tmp/yate-yrtpchan.conf /etc/yate/yrtpchan.conf && yate_need_restart=1
+}
+
+yate_openssl() {
+    cat >/tmp/yate-openssl.conf <<EOF
+[server]
+enable=yes
+certificate=server.pem
+EOF
+    oc_move /tmp/yate-openssl.conf /etc/yate/openssl.conf && yate_need_restart=1
+
+    if [ -n "$config_yate_cert" ]
+    then
+        echo -n "$config_yate_cert" | tail -n +2 >/tmp/yate-server.pem
+        chmod 640 /tmp/yate-server.pem
+        oc_move /tmp/yate-server.pem /etc/yate/server.pem && radiusd_need_restart=1
+    fi
 }
 
 yate_regfile() {
@@ -78,7 +100,7 @@ config rule 'rule_yate_sip'
   option src 'wan'
   option dest_port '${config_yate_sip_port:-5060}'
   option target 'ACCEPT'
-  option proto 'udp'
+  option proto 'tcpudp'
 
 config rule 'rule_yate_rtp'
   option name 'Allow-Yate-Rtp'
@@ -97,6 +119,7 @@ yate_need_restart=0
 yate_packages
 yate_ysipchan
 yate_yrtpchan
+[ "$config_yate_sip_type" = 'tls' ] && yate_openssl
 echo "$config_yate_users" | oc_strip_comment | yate_regfile && yate_need_restart=1
 yate_regexroute
 yate_firewall
