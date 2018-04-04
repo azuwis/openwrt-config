6c156aaa510ec90a6379f3fa02fc634b6da0e0f2
Zhong Jianxin <azuwis@gmail.com>
2018-04-04 10:30:21 +0800

    vlmcsd: Support ramips

--- local-050-vlmcsd.sh
+++ local-050-vlmcsd.sh
@@ -1,7 +1,11 @@
+download https://github.com/Wind4/vlmcsd/releases/download/svn1111/binaries.tar.gz
 if [ ! -e files/vlmcsd/vlmcsd-ar71xx ]; then
-    download https://github.com/Wind4/vlmcsd/releases/download/svn1107/binaries.tar.gz
     tar -xOf files/tmp/binaries.tar.gz binaries/Linux/mips/big-endian/musl/vlmcsd-mips16-openwrt-atheros-ar7xxx-ar9xxx-musl > files/vlmcsd/vlmcsd-ar71xx
     chmod +x files/vlmcsd/vlmcsd-ar71xx
+fi
+if [ ! -e files/vlmcsd/vlmcsd-ramips ]; then
+    tar -xOf files/tmp/binaries.tar.gz binaries/Linux/mips/little-endian/static/vlmcsd-mips16el-musl-static > files/vlmcsd/vlmcsd-ramips
+    chmod +x files/vlmcsd/vlmcsd-ramips
 fi
 
 push files/vlmcsd/init /etc/init.d/vlmcsd
