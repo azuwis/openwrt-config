2b96f76af6f57daa782f8202ded5eb5e996c58a0
Zhong Jianxin <azuwis@gmail.com>
2018-04-05 16:31:02 +0800

    adblock: Fix initialization

--- remote-070-adblock.sh
+++ remote-070-adblock.sh
@@ -1,6 +1,12 @@
 oc_opkg_install adblock ca-bundle libustream-openssl
 
-# uci set "adblock.global.adb_enabled=1"
-# oc_service restart adblock
+uci set "adblock.global.adb_enabled=1"
+if grep -q '^#' /etc/config/adblock
+then
+    uci commit
+    oc_service restart adblock -
+else
+    oc_service restart adblock
+fi
 
 oc_add_cron adblock '0 4 * * * /etc/init.d/adblock start'
