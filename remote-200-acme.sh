c615e0b9012f6f0b76215f72323d2ba9b07749ed
Zhong Jianxin <azuwis@gmail.com>
2018-04-12 22:21:27 +0800

    acme: Handle credentials in uci

--- remote-200-acme.sh
+++ remote-200-acme.sh
@@ -4,27 +4,6 @@ oc_uci_rename acme @acme[0] acme
 oc_uci_delete acme.example
 oc_service reload acme
 
-acme_account() {
-    local line key account='/etc/acme/account.conf'
-    while read -r line
-    do
-        key="${line%%=*}"
-        if grep -qFx "$line" "$account"
-        then
-            continue
-        else
-            if grep -q "^${key}=" "$account"
-            then
-                sed -i "s/^${key}=.*/${line}/" "$account"
-            else
-                echo "$line" >> "$account"
-            fi
-        fi
-    done
-}
-[ -e /etc/acme/account.conf ] || touch /etc/acme/account.conf
 chmod 750 /etc/acme
-chmod 640 /etc/acme/account.conf
-echo "$config_acme_account" | oc_strip_comment | acme_account
 
 oc_uci_merge "$config_acme"
