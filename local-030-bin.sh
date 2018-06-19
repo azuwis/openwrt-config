8a69cb39051dd184f06b67419b0b4dbd47a51116
Zhong Jianxin <azuwis@gmail.com>
2018-06-19 15:46:41 +0800

    bin: Add swshow

--- local-030-bin.sh
+++ local-030-bin.sh
@@ -7,3 +7,4 @@ push files/bin/profile.sh /etc/profile.d/oc-bin.sh
 push files/bin/ipcn /root/bin/ipcn
 push files/bin/ipinfo /root/bin/ipinfo
 push files/bin/lrcc /root/bin/lrcc
+push files/bin/swshow /root/bin/swshow
