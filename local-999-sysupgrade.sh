294e9197630bf82043f326098f54c6f279ef20e9
Zhong Jianxin <azuwis@gmail.com>
2020-02-13 19:46:30 +0800

    sysupgrade: Fix link

--- local-999-sysupgrade.sh
+++ local-999-sysupgrade.sh
@@ -1,6 +1,6 @@
-sysupgrade_url="https://downloads.lede-project.org/snapshots/targets/${arch_full}/openwrt-${arch_full/\//-}-${sysupgrade}-squashfs-sysupgrade.bin"
+sysupgrade_url="https://downloads.openwrt.org/snapshots/targets/${arch_full}/openwrt-${arch_full/\//-}-${sysupgrade}-squashfs-sysupgrade.bin"
 sysupgrade_img="$(basename "$sysupgrade_url")"
-sysupgrade_sha="https://downloads.lede-project.org/snapshots/targets/${arch_full}/sha256sums"
+sysupgrade_sha="https://downloads.openwrt.org/snapshots/targets/${arch_full}/sha256sums"
 download_push "$sysupgrade_url" "/tmp/${sysupgrade_img}"
 download "$sysupgrade_sha" "files/tmp/${sysupgrade_img}.sha256sums"
 if (cd files/tmp/; sha256sum -c --ignore-missing "${sysupgrade_img}.sha256sums"); then
