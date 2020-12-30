1b903f0d586a3980e5eab725d742853a03d26840
Zhong Jianxin <azuwis@gmail.com>
2020-12-30 15:18:46 +0800

    Add dsa script to show DSA switch vlan

--- local-030-bin.sh
+++ local-030-bin.sh
@@ -4,6 +4,7 @@ remote mkdir -p /root/bin/
 push files/bin/keep /lib/upgrade/keep.d/oc-bin
 push files/bin/profile.sh /etc/profile.d/oc-bin.sh
 
+push files/bin/dsa /root/bin/dsa
 push files/bin/ipcn /root/bin/ipcn
 push files/bin/ipinfo /root/bin/ipinfo
 push files/bin/lrcc /root/bin/lrcc
