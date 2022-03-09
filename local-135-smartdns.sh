505407988dd7a0d189155acc513da0b17b516e24
Zhong Jianxin <azuwis@gmail.com>
2022-03-09 15:45:19 +0800

    smartdns: Auto set local dns server

--- local-135-smartdns.sh
+++ local-135-smartdns.sh
@@ -5,8 +5,15 @@ chn() {
 if [ ! -e files/smartdns/address.conf ]; then
     download https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf
     chn < files/tmp/accelerated-domains.china.conf > files/smartdns/address.conf
-    cat files/smartdns/extra >> files/smartdns/address.conf
 fi
 
-push files/smartdns/address.conf /etc/smartdns/address.conf
+if [ ! -e files/tmp/address.conf.gz -o files/smartdns/address.conf -nt files/tmp/address.conf.gz ]; then
+    echo 'zipping files/smartdns/address.conf'
+    gzip -nc files/smartdns/address.conf > files/tmp/address.conf.gz
+fi
+
+remote mkdir -p /etc/smartdns
+push files/tmp/address.conf.gz /etc/smartdns/address.conf.gz
 push files/smartdns/custom.conf /etc/smartdns/custom.conf
+push files/smartdns/hotplug /etc/hotplug.d/iface/99-smartdns
+push files/smartdns/keep /lib/upgrade/keep.d/oc-smartdns
