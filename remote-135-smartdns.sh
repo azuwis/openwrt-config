72236be086951db3180cb68b0ad520e30f3f3724
Zhong Jianxin <azuwis@gmail.com>
2022-09-30 10:02:41 +0800

    smartdns: Update with upstream

--- remote-135-smartdns.sh
+++ remote-135-smartdns.sh
@@ -2,6 +2,6 @@ oc_opkg_install smartdns
 
 uci batch <<EOF
 set smartdns.@smartdns[0].enabled='1'
-set smartdns.@smartdns[0].redirect='dnsmasq-upstream'
+set smartdns.@smartdns[0].auto_set_dnsmasq='1'
 EOF
 oc_service restart smartdns
