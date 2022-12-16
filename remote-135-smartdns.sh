c55bf3be6ee7b3edbf63db30d51c988d7f93a955
Zhong Jianxin <azuwis@gmail.com>
2022-12-16 23:35:11 +0800

    smartdns: Set port and let dnsmasq use smartdns as upstream
    so <host>.lan will work

--- remote-135-smartdns.sh
+++ remote-135-smartdns.sh
@@ -3,5 +3,6 @@ oc_opkg_install smartdns
 uci batch <<EOF
 set smartdns.@smartdns[0].enabled='1'
 set smartdns.@smartdns[0].auto_set_dnsmasq='1'
+set smartdns.@smartdns[0].port='54'
 EOF
 oc_service restart smartdns
