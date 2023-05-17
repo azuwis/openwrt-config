b82c1b50e434b3643e0f61202c6ed4ae69ca4d50
Zhong Jianxin <azuwis@gmail.com>
2023-05-17 16:00:13 +0800

    Generate chn-cidr from 17mon

--- local-040-shadowsocks.sh
+++ local-040-shadowsocks.sh
@@ -1,9 +1,5 @@
-chn_cidr() {
-    awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' | sort -V | aggregate
-}
 if [ ! -e files/shadowsocks/chn-cidr ]; then
-    download http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
-    chn_cidr < files/tmp/delegated-apnic-latest > files/shadowsocks/chn-cidr
+    download https://github.com/17mon/china_ip_list/raw/master/china_ip_list.txt files/shadowsocks/chn-cidr
     cat >> files/shadowsocks/chn-cidr <<EOF
 64.62.200.2/32
 1.1.1.0/24
